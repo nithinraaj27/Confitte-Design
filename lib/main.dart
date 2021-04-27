e86b89654e16696e8fe368734848177075b575bf
nithinraaj27 <75876026+nithinraaj27@users.noreply.github.com>
2021-04-27 15:24:55 +0700

    Confitte Design in flutter

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.redAccent,
+        scaffoldBackgroundColor: Colors.redAccent,
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  ConfettiController controller1;
+
+  @override
+  void initState() {
+    controller1 = ConfettiController(duration: Duration(milliseconds: 500));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: ConfettiWidget(
+                    confettiController: controller1,
+                    shouldLoop: true,
+                    blastDirection: pi /2,
+                    gravity: 0.02,
+                    numberOfParticles: 100,
+                    emissionFrequency: 0.02,
+                    displayTarget: true,
+                    blastDirectionality: BlastDirectionality.explosive,
+                    colors: [
+                      Colors.redAccent,
+                      Colors.purpleAccent,
+                      Colors.pinkAccent,Colors.orange,
+                      Colors.orangeAccent
+                    ],
+                  ),
+                ),
+                // ignore: deprecated_member_use
+                FlatButton(
+                    onPressed: (){
+                      setState(() {
+                        controller1.play();
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context){
+                              return AlertDialog(
+                                content: Container(
+                                  height:100,
+                                  width:100,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      border: Border.all(width: 4,color: Colors.redAccent),
+                                      borderRadius: BorderRadius.circular(25),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            blurRadius: 25,
+                                            color: Colors.redAccent,
+                                            offset: Offset(1,15)
+                                        )
+                                      ]
+                                  ),
+                                  child: Text("HAPPY BIRTHDAY",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontStyle: FontStyle.italic,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 25
+                                    ),),
+                                ),
+                              );
+                            });
+
+                      });
+                    },
+                    child: Text("Press",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w800
+                      ),)
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
